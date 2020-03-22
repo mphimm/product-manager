7a50cdd07979db38464eb70e833bab57718ccc42
Millie Phimmasone <59859457+mphimm@users.noreply.github.com>
2020-03-21 22:38:46 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require ("./server/config/mongoose.config");
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+require("./server/routes/product.route")(app);
+
+app.listen (8000, () => {
+    console.log("listening at port 8000")
+});
+
