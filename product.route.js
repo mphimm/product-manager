a477f0446b96921b8d3944be42339073096eb7cc
Millie Phimmasone <59859457+mphimm@users.noreply.github.com>
2020-03-21 22:38:31 -0600

    Create product.route.js

---
+++ product.route.js
@@ -0,0 +1,9 @@
+const ProductController = require ("../controllers/product.controllers");
+
+module.exports = app => {
+    app.get("/api/products/", ProductController.findAllProducts);
+    app.get("/api/product/:id", ProductController.findOneProduct);
+    app.put("/api/product/update/:id", ProductController.updateOneProduct);
+    app.post("/api/product/new", ProductController.createNewProduct);
+    app.delete("/api/product/delete/:id", ProductController.deleteProduct);
+};
